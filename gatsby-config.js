6d4718b04463041885c3db037333b53bbd036c7c
rogersanbr <rsfernandes@outlook.com>
2020-12-21 17:09:10 -0300

    Adds initial structure

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: "Roger Santos",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-7PKJZWZK1K",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
