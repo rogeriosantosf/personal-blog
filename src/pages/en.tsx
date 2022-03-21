caad0ef9183fbbb7af5e8ac7ef0cc605819ace9f
Roger Santos <rsfernandes@outlook.com>
2022-03-21 13:42:13 -0300

    feat: add index page shared component

--- src/pages/en.tsx
+++ src/pages/en.tsx
@@ -1,89 +1,11 @@
-import React from "react"
-import { Link, graphql, PageProps } from "gatsby"
+import React from 'react'
+import { graphql, PageProps } from 'gatsby'
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Index, { DataProps } from '../components/pages'
 
-export type DataProps = {
-  site: {
-    siteMetadata: {
-      title: string
-    }
-  },
-  allMarkdownRemark: {
-    nodes: Array<{
-      excerpt: string,
-      fields: {
-        slug: string
-      },
-      frontmatter: {
-        date: string,
-        title: string,
-        description: string
-      }
-    }>
-  }
-}
-const IndexEnPage: React.FC<PageProps<DataProps>> = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+const BlogEnIndex = (props: PageProps<DataProps>) => <Index {...props} />
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-    </Layout>
-  )
-}
-
-export default IndexEnPage
+export default BlogEnIndex
 
 export const pageQuery = graphql`
   query IndexEnQuery {
